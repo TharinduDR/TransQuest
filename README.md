9ff31b8d502e5a02e4691bca148ef549fd97fe09
TharinduDR <rhtdranasinghe@gmail.com>
2020-03-13 15:48:47 +0000

    001: Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## TransQuest : Transformer based Translation Quality Estimation. 
+
+TransQuest provides state-of-the-art models for Quality Estimation.
+
--- trans_quest.py
+++ trans_quest.py
@@ -1,35 +1,37 @@
 import logging
 import os
-import torch
 
+import torch
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
 from algo.evaluation import pearson_corr, spearman_corr
 from algo.run_model import QuestModel
-from config.en_de_config import TEMP_DIRECTORY, MODEL_TYPE, MODEL_NAME, en_de_config, SEED, RESULT_FILE
+from config.global_config import TEMP_DIRECTORY, MODEL_TYPE, MODEL_NAME, global_config, SEED, RESULT_FILE
 from util.reader import read_files
 
 if not os.path.exists(TEMP_DIRECTORY):
     os.makedirs(TEMP_DIRECTORY)
 
-TRAIN_SRC_FILE = "data/Quality_Estimation/task1_en-de_traindev/train/train.src"
-TRAIN_TARGET_FILE = "data/Quality_Estimation/task1_en-de_traindev/train/train.mt"
-TRAIN_HTER_FILE = "data/Quality_Estimation/task1_en-de_traindev/train/train.hter"
 
-TEST_SRC_FILE = "data/Quality_Estimation/task1_en-de_traindev/dev/dev.src"
-TEST_TARGET_FILE = "data/Quality_Estimation/task1_en-de_traindev/dev/dev.mt"
-TEST_HTER_FILE = "data/Quality_Estimation/task1_en-de_traindev/dev/dev.hter"
+TRAIN_SRC_FILE = "data/Quality_Estimation/2018/sentence_level/en_lv/train.smt.src"
+TRAIN_TARGET_FILE = "data/Quality_Estimation/2018/sentence_level/en_lv/train.smt.mt"
+TRAIN_HTER_FILE = "data/Quality_Estimation/2018/sentence_level/en_lv/train.smt.hter"
+
+TEST_SRC_FILE = "data/Quality_Estimation/2018/sentence_level/en_lv/dev.smt.src"
+TEST_TARGET_FILE = "data/Quality_Estimation/2018/sentence_level/en_lv/dev.smt.mt"
+TEST_HTER_FILE = "data/Quality_Estimation/2018/sentence_level/en_lv/dev.smt.hter"
+
 
 model = QuestModel(MODEL_TYPE, MODEL_NAME, num_labels=1, use_cuda=torch.cuda.is_available(),
-                   args=en_de_config)
+                   args=global_config)
 
 train = read_files(TRAIN_SRC_FILE, TRAIN_TARGET_FILE, TRAIN_HTER_FILE)
 test = read_files(TEST_SRC_FILE, TEST_TARGET_FILE, TEST_HTER_FILE)
 
 logging.info("Started Training")
 
-if en_de_config["evaluate_during_training"]:
+if global_config["evaluate_during_training"]:
     train, eval_df = train_test_split(train, test_size=0.1, random_state=SEED)
     model.train_model(train, eval_df=eval_df, pearson_corr=pearson_corr, spearman_corr=spearman_corr,
                       mae=mean_absolute_error)
