ddd38a0d435fa5b75dec0d599a4ec1369b3fa4ef
TharinduDR <rhtdranasinghe@gmail.com>
2020-03-26 21:57:52 +0000

    004: Adding 2020 examples

--- examples/en_de/transformer_config.py
+++ examples/en_de/transformer_config.py
@@ -3,8 +3,8 @@ from multiprocessing import cpu_count
 SEED = 777
 TEMP_DIRECTORY = "temp/data"
 RESULT_FILE = "result.tsv"
-MODEL_TYPE = "xlm"
-MODEL_NAME = "xlm-clm-ende-1024"
+MODEL_TYPE = "xlmroberta"
+MODEL_NAME = "xlm-roberta-base"
 
 transformer_config = {
     'output_dir': 'temp/outputs/',
