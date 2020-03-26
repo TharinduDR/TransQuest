4b5a6114edb1f072f21f243e4f16a8f86159fe05
TharinduDR <rhtdranasinghe@gmail.com>
2020-03-26 18:13:29 +0000

    004: Adding 2020 examples

--- examples/ru_en/trans_quest.py
+++ examples/ru_en/trans_quest.py
@@ -15,8 +15,8 @@ from examples.ru_en.transformer_config import TEMP_DIRECTORY, MODEL_TYPE, MODEL_
 if not os.path.exists(TEMP_DIRECTORY):
     os.makedirs(TEMP_DIRECTORY)
 
-TRAIN_FILE = "../../data/ru-en/train.ruen.df.short.tsv"
-TEST_FILE = "../../data/ru-en/dev.ruen.df.short.tsv"
+TRAIN_FILE = "data/ru-en/train.ruen.df.short.tsv"
+TEST_FILE = "data/ru-en/dev.ruen.df.short.tsv"
 
 model = QuestModel(MODEL_TYPE, MODEL_NAME, num_labels=1, use_cuda=torch.cuda.is_available(),
                    args=transformer_config)
