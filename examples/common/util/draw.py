30ce7ba3dcfc4cbc2e16590c0ba5cdf269bf4b47
TharinduDR <rhtdranasinghe@gmail.com>
2020-03-30 10:33:28 +0100

    004: Adding 2020 examples

--- examples/common/util/draw.py
+++ examples/common/util/draw.py
@@ -1,14 +1,18 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
 sns.set()
 
 
 def draw_scatterplot(data_frame, real_column, prediction_column, path):
+    data_frame = data_frame.sort_values(real_column)
+    sort_id = list(range(0, len(data_frame.index)))
+    data_frame['id'] = pd.Series(sort_id).values
 
-    ax = sns.scatterplot(x="total_bill", y=real_column,
+    ax = sns.scatterplot(x="id", y=real_column,
                          data=data_frame)
-    ax = sns.scatterplot(x="total_bill", y=prediction_column,
+    ax = sns.scatterplot(x="id", y=prediction_column,
                          data=data_frame, ax=ax)
 
     fig = ax.get_figure()
