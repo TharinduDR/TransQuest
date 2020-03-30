8dc4992184386397381d4a9fd7993e155e6db89c
TharinduDR <rhtdranasinghe@gmail.com>
2020-03-30 11:20:10 +0100

    004: Adding 2020 examples

--- examples/common/util/draw.py
+++ examples/common/util/draw.py
@@ -10,9 +10,9 @@ def draw_scatterplot(data_frame, real_column, prediction_column, path, topic):
     sort_id = list(range(0, len(data_frame.index)))
     data_frame['id'] = pd.Series(sort_id).values
 
-    ax = data_frame.plot(kind='scatter', x='id', y=real_column, color='DarkBlue', label='Similarity', title=topic);
-    ax = data_frame.plot(kind='scatter', x='id', y=prediction_column, color='DarkGreen', label='Predicted Similarity',
-                    ax=ax);
+    ax = data_frame.plot(kind='scatter', x='id', y=real_column, color='DarkBlue', label='z_mean', title=topic)
+    ax = data_frame.plot(kind='scatter', x='id', y=prediction_column, color='DarkGreen', label='predicted z_mean',
+                    ax=ax)
     # ax.text(1500, 0.05, textstr, fontsize=12)
 
     fig = ax.get_figure()
