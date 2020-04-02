43a253707211630ab3fe0eb47f85aa7ad04a28db
TharinduDR <rhtdranasinghe@gmail.com>
2020-04-02 16:47:48 +0100

    005: Adding siamese architecture

---
+++ algo/siamese_transformers/losses/CosineSimilarityLoss.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn, Tensor
+from typing import Union, Tuple, List, Iterable, Dict
+from ..run_model import SiameseQuestModel
+
+class CosineSimilarityLoss(nn.Module):
+    def __init__(self, model: SiameseQuestModel):
+        super(CosineSimilarityLoss, self).__init__()
+        self.model = model
+
+
+    def forward(self, sentence_features: Iterable[Dict[str, Tensor]], labels: Tensor):
+        reps = [self.model(sentence_feature)['sentence_embedding'] for sentence_feature in sentence_features]
+        rep_a, rep_b = reps
+
+        output = torch.cosine_similarity(rep_a, rep_b)
+        loss_fct = nn.MSELoss()
+
+        if labels is not None:
+            loss = loss_fct(output, labels.view(-1))
+            return loss
+        else:
+            return reps, output
