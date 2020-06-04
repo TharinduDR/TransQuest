177b54a48c276ece44f81c421a95bc4cdaf205d8
TharinduDR <rhtdranasinghe@gmail.com>
2020-06-04 11:23:44 +0100

    010: Code Cleaning for Siamese Architectures

--- examples/wmt_2020/ro_en/siamese_trans_quest.py
+++ examples/wmt_2020/ro_en/siamese_trans_quest.py
@@ -51,12 +51,11 @@ train = train[['original', 'translation', 'z_mean']]
 dev = dev[['original', 'translation', 'z_mean']]
 test = test[['original', 'translation']]
 
+
 train = train.rename(columns={'original': 'text_a', 'translation': 'text_b', 'z_mean': 'labels'}).dropna()
 dev = dev.rename(columns={'original': 'text_a', 'translation': 'text_b', 'z_mean': 'labels'}).dropna()
 test = test.rename(columns={'original': 'text_a', 'translation': 'text_b'}).dropna()
 
-# dev_sentence_pairs = list(map(list, zip(dev['text_a'].to_list(), test['text_b'].to_list())))
-# test_sentence_pairs = list(map(list, zip(test['text_a'].to_list(), test['text_b'].to_list())))
 
 train = fit(train, 'labels')
 dev = fit(dev, 'labels')
@@ -152,64 +151,3 @@ draw_scatterplot(dev, 'labels', 'predictions', os.path.join(TEMP_DIRECTORY, RESU
 print_stat(dev, 'labels', 'predictions')
 format_submission(df=test, index=index, language_pair="ro-en", method="TransQuest", path=os.path.join(TEMP_DIRECTORY, SUBMISSION_FILE))
 
-# # Read the dataset
-# train_batch_size = 16
-# num_epochs = 4
-# model_save_path = 'output/training_stsbenchmark_bert-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-# sts_reader = STSDataReader('examples/wmt_2020/ro_en/data/ro-en/', s1_col_idx=1, s2_col_idx=2, score_col_idx=6, normalize_scores=True, header=True)
-#
-# # Use BERT for mapping tokens to embeddings
-# word_embedding_model = models.Transformer('xlm-roberta-base')
-#
-# # Apply mean pooling to get one fixed sized sentence vector
-# pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
-#                                pooling_mode_mean_tokens=True,
-#                                pooling_mode_cls_token=False,
-#                                pooling_mode_max_tokens=False)
-#
-# model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
-#
-# # Convert the dataset to a DataLoader ready for training
-# logging.info("Read STSbenchmark train dataset")
-# train_data = SentencesDataset(sts_reader.get_examples('train.roen.df.short.tsv'), model)
-# train_dataloader = DataLoader(train_data, shuffle=True, batch_size=train_batch_size)
-# train_loss = losses.CosineSimilarityLoss(model=model)
-#
-#
-# logging.info("Read STSbenchmark dev dataset")
-# dev_data = SentencesDataset(examples=sts_reader.get_examples('dev.roen.df.short.tsv'), model=model)
-# dev_dataloader = DataLoader(dev_data, shuffle=False, batch_size=train_batch_size)
-# evaluator = EmbeddingSimilarityEvaluator(dev_dataloader)
-#
-#
-# # Configure the training. We skip evaluation in this example
-# warmup_steps = math.ceil(len(train_data)*num_epochs/train_batch_size*0.1) #10% of train data for warm-up
-# logging.info("Warmup-steps: {}".format(warmup_steps))
-#
-#
-# # Train the model
-# model.fit(train_objectives=[(train_dataloader, train_loss)],
-#           evaluator=evaluator,
-#           epochs=num_epochs,
-#           evaluation_steps=1000,
-#           warmup_steps=warmup_steps,
-#           output_path=model_save_path)
-
-
-##############################################################################
-#
-# Load the stored model and evaluate its performance on STS benchmark dataset
-#
-##############################################################################
-
-# model = SentenceTransformer(model_save_path)
-# test = read_test_file("examples/wmt_2020/ro_en/data/ro-en/test20.roen.df.short.tsv")
-# test_sentence_pairs = list(map(list, zip(test['text_a'].to_list(), test['text_b'].to_list())))
-# predictions = model.predict(test_sentence_pairs)
-# test['predictions'] = predictions
-
-
-# test_data = SentencesDataset(examples=sts_reader.get_examples("test20.roen.df.short.tsv"), model=model)
-# test_dataloader = DataLoader(test_data, shuffle=False, batch_size=train_batch_size)
-# evaluator = EmbeddingSimilarityEvaluator(test_dataloader)
-# model.evaluate(evaluator)
