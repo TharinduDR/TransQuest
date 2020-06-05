4017c3d187f1cd08f2cce6dfbcc836d449995aca
TharinduDR <rhtdranasinghe@gmail.com>
2020-06-05 15:42:42 +0100

    019: Added wmt 2020 examples

--- examples/wmt_2019/en_de/trans_quest.py
+++ examples/wmt_2019/en_de/trans_quest.py
@@ -2,7 +2,6 @@ import os
 import shutil
 
 import numpy as np
-import pandas as pd
 import torch
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
@@ -12,7 +11,7 @@ from examples.wmt_2019.common.util.draw import draw_scatterplot, print_stat
 from examples.wmt_2019.common.util.normalizer import fit, un_fit
 from examples.wmt_2019.common.util.reader import read_annotated_file, read_test_file
 from examples.wmt_2019.en_de.transformer_config import TEMP_DIRECTORY, GOOGLE_DRIVE, DRIVE_FILE_ID, MODEL_NAME, \
-    transformer_config, MODEL_TYPE, SEED, RESULT_FILE, SUBMISSION_FILE, RESULT_IMAGE
+    transformer_config, MODEL_TYPE, SEED, RESULT_FILE, RESULT_IMAGE
 from transquest.algo.transformers.evaluation import pearson_corr, spearman_corr
 from transquest.algo.transformers.run_model import QuestModel
 
