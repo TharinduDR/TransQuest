e0040788de736cb08d31e2f1b83ca5fcd76b56da
TharinduDR <rhtdranasinghe@gmail.com>
2020-06-08 19:20:59 +0100

    021: Preparing release 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="transquest",
-    version="0.1.0",
+    version="0.2.0",
     author="Tharindu Ranasinghe",
     author_email="rhtdranasinghe@gmail.com",
     description="Transformer based translation quality estimation",
