eddf571eede1897cdb5d885a3886df08b8e8c87c
TharinduDR <rhtdranasinghe@gmail.com>
2020-06-10 17:54:06 +0100

    023: Preparing data for TransQuest ++

--- examples/wmt_2020/si_en/transformer_config.py
+++ examples/wmt_2020/si_en/transformer_config.py
@@ -5,10 +5,10 @@ TEMP_DIRECTORY = "temp/data"
 RESULT_FILE = "result.tsv"
 RESULT_IMAGE = "result.jpg"
 SUBMISSION_FILE = "predictions.txt"
-GOOGLE_DRIVE = False
-DRIVE_FILE_ID = None
+GOOGLE_DRIVE = True
+DRIVE_FILE_ID = "1-U0C1qc7elkpMRbQzDBAAekqor7Mbn7X"
 MODEL_TYPE = "xlmroberta"
-MODEL_NAME = "xlm-roberta-large"
+MODEL_NAME = "transquest/ne-en"
 
 transformer_config = {
     'output_dir': 'temp/outputs/',
