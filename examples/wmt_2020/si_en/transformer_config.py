781c89ea9a1bef9f2840d2e0c1923e788af0c680
TharinduDR <rhtdranasinghe@gmail.com>
2020-06-24 15:16:51 +0100

    030: Adding multilingual experiments

--- examples/wmt_2020/si_en/transformer_config.py
+++ examples/wmt_2020/si_en/transformer_config.py
@@ -5,10 +5,10 @@ TEMP_DIRECTORY = "temp/data"
 RESULT_FILE = "result.tsv"
 RESULT_IMAGE = "result.jpg"
 SUBMISSION_FILE = "predictions.txt"
-GOOGLE_DRIVE = True
+GOOGLE_DRIVE = False
 DRIVE_FILE_ID = "1-U0C1qc7elkpMRbQzDBAAekqor7Mbn7X"
 MODEL_TYPE = "xlmroberta"
-MODEL_NAME = "transquest/ne-en"
+MODEL_NAME = "xlm-roberta-large"
 
 transformer_config = {
     'output_dir': 'temp/outputs/',
