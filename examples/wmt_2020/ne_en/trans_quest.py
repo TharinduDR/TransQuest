400a5dfde7c18331b0923b33f322b951d2faf779
TharinduDR <rhtdranasinghe@gmail.com>
2020-06-24 17:12:27 +0100

    030: Adding multilingual experiments

--- examples/wmt_2020/ne_en/trans_quest.py
+++ examples/wmt_2020/ne_en/trans_quest.py
@@ -40,7 +40,7 @@ test_sentence_pairs = list(map(list, zip(test['text_a'].to_list(), test['text_b'
 train = fit(train, 'labels')
 dev = fit(dev, 'labels')
 
-train = train[:1000]
+train = train[:2000]
 
 assert (len(index) == 1000)
 if transformer_config["evaluate_during_training"]:
