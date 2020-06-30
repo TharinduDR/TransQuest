d7ea057199286081674bfd4dfcfad434fb26de2b
TharinduDR <rhtdranasinghe@gmail.com>
2020-06-30 22:02:13 +0100

    030: Adding multilingual experiments

--- examples/wmt_2020/ru_en/trans_quest.py
+++ examples/wmt_2020/ru_en/trans_quest.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-
+import pandas as pd
 import numpy as np
 import torch
 from sklearn.metrics import mean_absolute_error
@@ -44,6 +44,8 @@ test_sentence_pairs = list(map(list, zip(test['text_a'].to_list(), test['text_b'
 
 train = fit(train, 'labels')
 dev = fit(dev, 'labels')
+
+train = pd.concat([train, dev])
 
 if transformer_config["evaluate_during_training"]:
     if transformer_config["n_fold"] > 1:
