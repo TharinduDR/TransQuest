f1f42f253ad90fec9394aabaff88ee43c64100b3
TharinduDR <rhtdranasinghe@gmail.com>
2020-07-03 18:25:59 +0100

    030: Adding multilingual experiments

--- examples/wmt_2020/en_de/transformer_config.py
+++ examples/wmt_2020/en_de/transformer_config.py
@@ -21,7 +21,7 @@ transformer_config = {
     'train_batch_size': 8,
     'gradient_accumulation_steps': 1,
     'eval_batch_size': 8,
-    'num_train_epochs': 4,
+    'num_train_epochs': 3,
     'weight_decay': 0,
     'learning_rate': 1e-5,
     'adam_epsilon': 1e-8,
