043b2728f167c59a8e51ddfd2400d84cdf362c46
TharinduDR <rhtdranasinghe@gmail.com>
2020-07-15 10:26:06 +0100

    031: Fine tunning

--- examples/wmt_2020/si_en/transformer_config.py
+++ examples/wmt_2020/si_en/transformer_config.py
@@ -5,10 +5,10 @@ TEMP_DIRECTORY = "temp/data"
 RESULT_FILE = "result.tsv"
 RESULT_IMAGE = "result.jpg"
 SUBMISSION_FILE = "predictions.txt"
-GOOGLE_DRIVE = True
+GOOGLE_DRIVE = False
 DRIVE_FILE_ID = "1F-hknKCY0spD8H1gfhE9EcxusEze2si2"
 MODEL_TYPE = "xlmroberta"
-MODEL_NAME = "transquest/ro-en"
+MODEL_NAME = "xlm-roberta-large"
 
 transformer_config = {
     'output_dir': 'temp/outputs/',
