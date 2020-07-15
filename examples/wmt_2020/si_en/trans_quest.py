b9c64d96a41ac4113857970ce44fc236a817b62b
TharinduDR <rhtdranasinghe@gmail.com>
2020-07-15 10:18:59 +0100

    031: Fine tunning

--- examples/wmt_2020/si_en/trans_quest.py
+++ examples/wmt_2020/si_en/trans_quest.py
@@ -45,7 +45,7 @@ test_sentence_pairs = list(map(list, zip(test['text_a'].to_list(), test['text_b'
 train = fit(train, 'labels')
 dev = fit(dev, 'labels')
 
-train = pd.concat([train,dev])
+train = pd.concat([train, dev])
 
 assert (len(index) == 1000)
 if transformer_config["evaluate_during_training"]:
