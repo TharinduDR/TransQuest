8d0a087d67157ece42cac5a60d6de7ef320e61ad
TharinduDR <rhtdranasinghe@gmail.com>
2020-12-30 20:19:59 +0000

    053: Project Restructuring

--- examples/sentence_level/wmt_2020/ro_en/monotransquest.py
+++ examples/sentence_level/wmt_2020/ro_en/monotransquest.py
@@ -8,14 +8,14 @@ import torch
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
-from examples.wmt_2020.common.util.download import download_from_google_drive
-from examples.wmt_2020.common.util.draw import draw_scatterplot, print_stat
-from examples.wmt_2020.common.util.normalizer import fit, un_fit
-from examples.wmt_2020.common.util.postprocess import format_submission
-from examples.wmt_2020.common.util.reader import read_annotated_file, read_test_file
-from examples.wmt_2020.ro_en.transformer_config import TEMP_DIRECTORY, MODEL_TYPE, MODEL_NAME, transformer_config, SEED, \
+from examples.sentence_level.wmt_2020.common.util.download import download_from_google_drive
+from examples.sentence_level.wmt_2020.common.util.draw import draw_scatterplot, print_stat
+from examples.sentence_level.wmt_2020.common.util.normalizer import fit, un_fit
+from examples.sentence_level.wmt_2020.common.util.postprocess import format_submission
+from examples.sentence_level.wmt_2020.common.util.reader import read_annotated_file, read_test_file
+from examples.sentence_level.wmt_2020.ro_en.monotransquest_config import TEMP_DIRECTORY, MODEL_TYPE, MODEL_NAME, monotransquest_config, SEED, \
     RESULT_FILE, RESULT_IMAGE, GOOGLE_DRIVE, DRIVE_FILE_ID, SUBMISSION_FILE
-from transquest.algo.monotransquest.evaluation import pearson_corr, spearman_corr
+from transquest.algo.sentence_level.monotransquest.evaluation import pearson_corr, spearman_corr
 from transquest.algo.sentence_level.monotransquest.run_model import QuestModel
 
 if not os.path.exists(TEMP_DIRECTORY):
@@ -24,9 +24,9 @@ if not os.path.exists(TEMP_DIRECTORY):
 if GOOGLE_DRIVE:
     download_from_google_drive(DRIVE_FILE_ID, MODEL_NAME)
 
-TRAIN_FILE = "examples/wmt_2020/ro_en/data/ro-en/train.roen.df.short.tsv"
-DEV_FILE = "examples/wmt_2020/ro_en/data/ro-en/dev.roen.df.short.tsv"
-TEST_FILE = "examples/wmt_2020/ro_en/data/ro-en/test20.roen.df.short.tsv"
+TRAIN_FILE = "examples/sentence_level/wmt_2020/ro_en/data/ro-en/train.roen.df.short.tsv"
+DEV_FILE = "examples/sentence_level/wmt_2020/ro_en/data/ro-en/dev.roen.df.short.tsv"
+TEST_FILE = "examples/sentence_level/wmt_2020/ro_en/data/ro-en/test20.roen.df.short.tsv"
 
 train = read_annotated_file(TRAIN_FILE)
 dev = read_annotated_file(DEV_FILE)
@@ -48,24 +48,24 @@ dev = fit(dev, 'labels')
 
 
 assert (len(index) == 1000)
-if transformer_config["evaluate_during_training"]:
-    if transformer_config["n_fold"] > 1:
-        dev_preds = np.zeros((len(dev), transformer_config["n_fold"]))
-        test_preds = np.zeros((len(test), transformer_config["n_fold"]))
-        for i in range(transformer_config["n_fold"]):
+if monotransquest_config["evaluate_during_training"]:
+    if monotransquest_config["n_fold"] > 1:
+        dev_preds = np.zeros((len(dev), monotransquest_config["n_fold"]))
+        test_preds = np.zeros((len(test), monotransquest_config["n_fold"]))
+        for i in range(monotransquest_config["n_fold"]):
 
-            if os.path.exists(transformer_config['output_dir']) and os.path.isdir(transformer_config['output_dir']):
-                shutil.rmtree(transformer_config['output_dir'])
+            if os.path.exists(monotransquest_config['output_dir']) and os.path.isdir(monotransquest_config['output_dir']):
+                shutil.rmtree(monotransquest_config['output_dir'])
 
             model = QuestModel(MODEL_TYPE, MODEL_NAME, num_labels=1, use_cuda=torch.cuda.is_available(),
-                               args=transformer_config)
+                               args=monotransquest_config)
             train_df, eval_df = train_test_split(train, test_size=0.1, random_state=SEED * i)
 
             model.train_model(train_df, eval_df=eval_df, pearson_corr=pearson_corr, spearman_corr=spearman_corr,
                               mae=mean_absolute_error)
 
-            model = QuestModel(MODEL_TYPE, transformer_config["best_model_dir"], num_labels=1,
-                               use_cuda=torch.cuda.is_available(), args=transformer_config)
+            model = QuestModel(MODEL_TYPE, monotransquest_config["best_model_dir"], num_labels=1,
+                               use_cuda=torch.cuda.is_available(), args=monotransquest_config)
 
             result, model_outputs, wrong_predictions = model.eval_model(dev, pearson_corr=pearson_corr,
                                                                         spearman_corr=spearman_corr,
@@ -80,7 +80,7 @@ if transformer_config["evaluate_during_training"]:
 
     else:
         model = QuestModel(MODEL_TYPE, MODEL_NAME, num_labels=1, use_cuda=torch.cuda.is_available(),
-                           args=transformer_config)
+                           args=monotransquest_config)
         train_df, eval_df = train_test_split(train, test_size=0.1, random_state=SEED)
 
         start = time.time()
@@ -90,8 +90,8 @@ if transformer_config["evaluate_during_training"]:
         print("Training time")
         print(end - start)
 
-        model = QuestModel(MODEL_TYPE, transformer_config["best_model_dir"], num_labels=1,
-                           use_cuda=torch.cuda.is_available(), args=transformer_config)
+        model = QuestModel(MODEL_TYPE, monotransquest_config["best_model_dir"], num_labels=1,
+                           use_cuda=torch.cuda.is_available(), args=monotransquest_config)
         result, model_outputs, wrong_predictions = model.eval_model(dev, pearson_corr=pearson_corr,
                                                                     spearman_corr=spearman_corr,
                                                                     mae=mean_absolute_error)
@@ -105,7 +105,7 @@ if transformer_config["evaluate_during_training"]:
 
 else:
     model = QuestModel(MODEL_TYPE, MODEL_NAME, num_labels=1, use_cuda=torch.cuda.is_available(),
-                       args=transformer_config)
+                       args=monotransquest_config)
     model.train_model(train, pearson_corr=pearson_corr, spearman_corr=spearman_corr, mae=mean_absolute_error)
     result, model_outputs, wrong_predictions = model.eval_model(dev, pearson_corr=pearson_corr,
                                                                 spearman_corr=spearman_corr, mae=mean_absolute_error)
--- examples/sentence_level/wmt_2020/ro_en/monotransquest_config.py
+++ examples/sentence_level/wmt_2020/ro_en/monotransquest_config.py
@@ -2,15 +2,15 @@ from multiprocessing import cpu_count
 
 SEED = 777
 TEMP_DIRECTORY = "temp/data"
-RESULT_FILE = "result_nmt.txt"
+RESULT_FILE = "result.tsv"
 SUBMISSION_FILE = "predictions.txt"
 RESULT_IMAGE = "result.jpg"
-GOOGLE_DRIVE = True
-DRIVE_FILE_ID = "1-VvMh_Pto0idpSm18yfoBfxUT3Orl3DJ"
+GOOGLE_DRIVE = False
+DRIVE_FILE_ID = None
 MODEL_TYPE = "xlmroberta"
-MODEL_NAME = "models/transquest-ro-en"
+MODEL_NAME = "xlm-roberta-large"
 
-transformer_nmt_config = {
+monotransquest_config = {
     'output_dir': 'temp/outputs/',
     "best_model_dir": "temp/outputs/best_model",
     'cache_dir': 'temp/cache_dir/',
@@ -20,12 +20,12 @@ transformer_nmt_config = {
     'max_seq_length': 80,
     'train_batch_size': 8,
     'gradient_accumulation_steps': 1,
-    'eval_batch_size': 1000,
+    'eval_batch_size': 8,
     'num_train_epochs': 3,
     'weight_decay': 0,
     'learning_rate': 2e-5,
     'adam_epsilon': 1e-8,
-    'warmup_ratio': 0.06,
+    'warmup_ratio': 0.1,
     'warmup_steps': 0,
     'max_grad_norm': 1.0,
     'do_lower_case': False,
@@ -33,14 +33,19 @@ transformer_nmt_config = {
     'logging_steps': 300,
     'save_steps': 300,
     "no_cache": False,
-    'save_model_every_epoch': True,
+    "no_save": False,
+    "save_recent_only": True,
+    'save_model_every_epoch': False,
     'n_fold': 3,
     'evaluate_during_training': True,
+    "evaluate_during_training_silent": False,
     'evaluate_during_training_steps': 300,
     "evaluate_during_training_verbose": True,
     'use_cached_eval_features': False,
-    'save_eval_checkpoints': True,
+    "save_best_model": True,
+    'save_eval_checkpoints': False,
     'tensorboard_dir': None,
+    "save_optimizer_and_scheduler": True,
 
     'regression': True,
 
@@ -50,6 +55,7 @@ transformer_nmt_config = {
     'process_count': cpu_count() - 2 if cpu_count() > 2 else 1,
     'n_gpu': 1,
     'use_multiprocessing': True,
+    "multiprocessing_chunksize": 500,
     'silent': False,
 
     'wandb_project': None,
@@ -60,8 +66,11 @@ transformer_nmt_config = {
     "early_stopping_delta": 0,
     "early_stopping_metric": "eval_loss",
     "early_stopping_metric_minimize": True,
+    "early_stopping_consider_epochs": False,
 
-    "manual_seed": 777,
+    "manual_seed": SEED,
 
+    "config": {},
+    "local_rank": -1,
     "encoding": None,
 }
