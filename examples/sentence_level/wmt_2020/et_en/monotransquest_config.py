4f4bde7d07455fee936b8602cecb9edcbe0e6c62
TharinduDR <rhtdranasinghe@gmail.com>
2020-12-30 17:35:55 +0000

    053: Project Restructuring

--- examples/sentence_level/wmt_2020/et_en/monotransquest_config.py
+++ examples/sentence_level/wmt_2020/et_en/monotransquest_config.py
@@ -3,29 +3,29 @@ from multiprocessing import cpu_count
 SEED = 777
 TEMP_DIRECTORY = "temp/data"
 RESULT_FILE = "result.tsv"
-SUBMISSION_FILE = "predictions.txt"
 RESULT_IMAGE = "result.jpg"
+SUBMISSION_FILE = "predictions.txt"
 GOOGLE_DRIVE = False
-DRIVE_FILE_ID = None
+DRIVE_FILE_ID = "1F-hknKCY0spD8H1gfhE9EcxusEze2si2"
 MODEL_TYPE = "xlmroberta"
-MODEL_NAME = "xlm-roberta-large"
+MODEL_NAME = "transquest/ro-en"
 
-siamese_transformer_config = {
+monotransquest_config = {
     'output_dir': 'temp/outputs/',
     "best_model_dir": "temp/outputs/best_model",
     'cache_dir': 'temp/cache_dir/',
 
     'fp16': False,
     'fp16_opt_level': 'O1',
-    'max_seq_length': 80,
+    'max_seq_length': 128,
     'train_batch_size': 8,
     'gradient_accumulation_steps': 1,
     'eval_batch_size': 8,
-    'num_train_epochs': 6,
+    'num_train_epochs': 3,
     'weight_decay': 0,
-    'learning_rate': 1e-5,
+    'learning_rate': 2e-5,
     'adam_epsilon': 1e-8,
-    'warmup_ratio': 0.06,
+    'warmup_ratio': 0.1,
     'warmup_steps': 0,
     'max_grad_norm': 1.0,
     'do_lower_case': False,
@@ -33,14 +33,19 @@ siamese_transformer_config = {
     'logging_steps': 300,
     'save_steps': 300,
     "no_cache": False,
-    'save_model_every_epoch': True,
-    'n_fold': 1,
+    "no_save": False,
+    "save_recent_only": True,
+    'save_model_every_epoch': False,
+    'n_fold': 3,
     'evaluate_during_training': True,
+    "evaluate_during_training_silent": False,
     'evaluate_during_training_steps': 300,
     "evaluate_during_training_verbose": True,
     'use_cached_eval_features': False,
+    "save_best_model": True,
     'save_eval_checkpoints': True,
     'tensorboard_dir': None,
+    "save_optimizer_and_scheduler": True,
 
     'regression': True,
 
@@ -50,6 +55,7 @@ siamese_transformer_config = {
     'process_count': cpu_count() - 2 if cpu_count() > 2 else 1,
     'n_gpu': 1,
     'use_multiprocessing': True,
+    "multiprocessing_chunksize": 500,
     'silent': False,
 
     'wandb_project': None,
@@ -60,8 +66,11 @@ siamese_transformer_config = {
     "early_stopping_delta": 0,
     "early_stopping_metric": "eval_loss",
     "early_stopping_metric_minimize": True,
+    "early_stopping_consider_epochs": False,
 
-    "manual_seed": 777,
+    "manual_seed": SEED,
 
+    "config": {},
+    "local_rank": -1,
     "encoding": None,
 }
