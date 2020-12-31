c63caff421369f8fa91820c03b0b017ef349741a
TharinduDR <rhtdranasinghe@gmail.com>
2020-12-31 00:02:37 +0000

    053: Project Restructuring

--- examples/sentence_level/wmt_2020/ro_en/siamesetransquest_config.py
+++ examples/sentence_level/wmt_2020/ro_en/siamesetransquest_config.py
@@ -10,7 +10,7 @@ DRIVE_FILE_ID = None
 MODEL_TYPE = "xlmroberta"
 MODEL_NAME = "xlm-roberta-large"
 
-siamesetraansquest_config = {
+siamesetransquest_config = {
     'output_dir': 'temp/outputs/',
     "best_model_dir": "temp/outputs/best_model",
     'cache_dir': 'temp/cache_dir/',
