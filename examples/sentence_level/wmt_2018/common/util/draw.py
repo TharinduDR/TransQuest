b0cfc3e4daa77250036dd10dd891dbb95fed0f12
TharinduDR <rhtdranasinghe@gmail.com>
2020-12-31 00:08:27 +0000

    053: Project Restructuring

--- examples/sentence_level/wmt_2018/common/util/draw.py
+++ examples/sentence_level/wmt_2018/common/util/draw.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.metrics import mean_absolute_error
 
 from examples.sentence_level.wmt_2018 import fit
-from transquest.algo.monotransquest.evaluation import pearson_corr, spearman_corr, rmse
+from transquest.algo.sentence_level.monotransquest.evaluation import pearson_corr, spearman_corr, rmse
 
 import matplotlib.pyplot as plt
 
