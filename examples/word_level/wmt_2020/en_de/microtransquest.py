1e6561e67a18c5f7a7e0f0a714de45dde9a840bc
TharinduDR <rhtdranasinghe@gmail.com>
2021-01-06 12:25:15 +0000

    053: Project Restructuring

--- examples/word_level/wmt_2020/en_de/microtransquest.py
+++ examples/word_level/wmt_2020/en_de/microtransquest.py
@@ -1,12 +1,17 @@
 from sklearn.model_selection import train_test_split
-
+import os
 from examples.word_level.common.util import reader
 from examples.word_level.wmt_2020.en_de.microtransquest_config import TRAIN_PATH, TRAIN_SOURCE_FILE, \
     TRAIN_SOURCE_TAGS_FILE, \
     TRAIN_TARGET_FILE, \
-    TRAIN_TARGET_TAGS_FLE, MODEL_TYPE, MODEL_NAME, microtransquest_config, TEST_PATH, TEST_SOURCE_FILE, TEST_TARGET_FILE
+    TRAIN_TARGET_TAGS_FLE, MODEL_TYPE, MODEL_NAME, microtransquest_config, TEST_PATH, TEST_SOURCE_FILE, \
+    TEST_TARGET_FILE, TEMP_DIRECTORY, TEST_SOURCE_TAGS_FILE, TEST_TARGET_TAGS_FLE
 from transquest.algo.word_level.microtransquest.run_model import MicroTransQuestModel
 from transquest.algo.word_level.microtransquest.format import prepare_data, prepare_testdata, post_process
+
+
+if not os.path.exists(TEMP_DIRECTORY):
+    os.makedirs(TEMP_DIRECTORY)
 
 train_source_sentences, train_target_sentences, train_source_tags, train_target_tags = reader(TRAIN_PATH, TRAIN_SOURCE_FILE, TRAIN_TARGET_FILE, TRAIN_SOURCE_TAGS_FILE, TRAIN_TARGET_TAGS_FLE)
 test_source_sentences, test_target_sentences = reader(TEST_PATH, TEST_SOURCE_FILE, TEST_TARGET_FILE)
@@ -31,6 +36,19 @@ test_sentences = prepare_testdata(test_source_sentences, test_target_sentences)
 predicted_labels = model.predict(test_sentences, split_on_space=True)
 
 sources_tags, targets_tags = post_process(predicted_labels)
+
+with open(os.path.join(TEMP_DIRECTORY, TEST_SOURCE_TAGS_FILE), 'w') as f:
+    for _list in sources_tags:
+        for _string in _list:
+            f.write(str(_string) + ' ')
+        f.write(str('\n'))
+
+
+with open(os.path.join(TEMP_DIRECTORY, TEST_TARGET_TAGS_FLE), 'w') as f:
+    for _list in targets_tags:
+        for _string in _list:
+            f.write(str(_string) + ' ')
+        f.write(str('\n'))
 
 
 
