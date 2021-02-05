21403d2f9a72ef329546bdc521a1e56e8564aec3
TharinduDR <rhtdranasinghe@gmail.com>
2021-02-05 20:22:53 +0000

    055: Adding word level examples

--- transquest/app/monotransquest_app.py
+++ transquest/app/monotransquest_app.py
@@ -4,8 +4,8 @@ from tqdm.auto import tqdm, trange
 from transquest.algo.sentence_level.monotransquest.run_model import MonoTransQuestModel
 from transquest.app.util.model_downloader import GoogleDriveDownloader as gdd
 
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-# logger.setLevel(logging.INFO)
 
 
 class MonoTransQuestApp:
