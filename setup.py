3abcb6720f0f1efba0db79e13dae87c189e583f1
TharinduDR <rhtdranasinghe@gmail.com>
2021-03-20 00:20:53 +0000

    056: Code Refactoring

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="transquest",
-    version="1.0.2-beta",
+    version="1.0.0",
     author="Tharindu Ranasinghe",
     author_email="rhtdranasinghe@gmail.com",
     description="Transformer based translation quality estimation",
