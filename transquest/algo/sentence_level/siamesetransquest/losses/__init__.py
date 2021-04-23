d9eac1cb3010009c0aa7f754f565470b4920b123
TharinduDR <rhtdranasinghe@gmail.com>
2021-04-23 20:07:00 +0100

    057: Code cleaning

--- transquest/algo/sentence_level/siamesetransquest/losses/__init__.py
+++ transquest/algo/sentence_level/siamesetransquest/losses/__init__.py
@@ -1,6 +1,1 @@
-# from .batch_hard_triplet_loss import *
-# from .cosine_similarity_loss import *
-# from .mse_loss import *
-# from .multiple_negatives_ranking_loss import *
-# from .softmax_loss import *
-# from .triplet_loss import *
+from .cosine_similarity_loss import *
