b0892efb7ef6686020b6431f193cca59e7311aee
TharinduDR <rhtdranasinghe@gmail.com>
2021-04-26 14:58:02 +0100

    057: Code Refactoring - Siamese Architectures

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="transquest",
-    version="1.0.2",
+    version="1.1.0",
     author="Tharindu Ranasinghe",
     author_email="rhtdranasinghe@gmail.com",
     description="Transformer based translation quality estimation",
