cf2d73cdce60f31d15b07148785a7adaf662ef1c
TharinduDR <rhtdranasinghe@gmail.com>
2023-07-20 17:12:56 +0100

    058: Adding large models

--- examples/sentence_level/wmt_2020/ro_en/monotransquest_config.py
+++ examples/sentence_level/wmt_2020/ro_en/monotransquest_config.py
@@ -8,7 +8,7 @@ RESULT_IMAGE = "result.jpg"
 GOOGLE_DRIVE = False
 DRIVE_FILE_ID = None
 MODEL_TYPE = "auto"
-MODEL_NAME = "facebook/xlm-roberta-xl"
+MODEL_NAME = "microsoft/infoxlm-large"
 
 monotransquest_config = {
     'output_dir': 'temp/outputs/',
@@ -18,7 +18,7 @@ monotransquest_config = {
     'fp16': False,
     'fp16_opt_level': 'O1',
     'max_seq_length': 80,
-    'train_batch_size': 4,
+    'train_batch_size': 8,
     'gradient_accumulation_steps': 1,
     'eval_batch_size': 8,
     'num_train_epochs': 3,
